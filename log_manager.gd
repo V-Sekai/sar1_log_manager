2746b5568cd16433237d310552f1f4bfb87890ad
Lyuma <xn.lyuma@gmail.com>
2022-03-10 13:43:52 -0800

    Migrate signals from strings to .emit() and .connect()

--- log_manager.gd
+++ log_manager.gd
@@ -6,16 +6,16 @@ signal error_hook(p_string)
 signal fatal_error_hook(p_string)
 
 func printl(p_line_string):
-	emit_signal("printl_hook", p_line_string)
+	printl_hook.emit(p_line_string)
 
 
 func error(p_error_string):
-	emit_signal("error_hook", p_error_string)
+	error_hook.emit(p_error_string)
 
 
 func fatal_error(p_error_string):
 	push_error(p_error_string)
-	emit_signal("fatal_error_hook", p_error_string)
+	fatal_error_hook.emit(p_error_string)
 	
 	if not Engine.is_editor_hint():
 		get_tree().quit()  #default behavior
